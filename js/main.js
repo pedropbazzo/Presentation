75a53ea6893ef907ec130304600a5571e691484f
Pedro Paulo Bazzo <pedropbazzo@icloud.com>
2024-01-02 08:47:14 -0300

    named fixed

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 //Type writing efeito
 $('#typewriteText').typewrite({
     actions: [
-       {type:'Ola, Sou Pedro Bazzo, Dev Lead, trabalhando diariamente para grandes bancos e empresas em diversos segmentos, ao longo da carreira, desde 2010.'},
+       {type:'Ola, Sou Pedro Bazzo, trabalho diariamente para grandes bancos e empresas em diversos segmentos, ao longo da carreira, desde 2010.'},
     ]
 })
 
